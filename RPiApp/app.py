f73652e81b33486e59bd12005f49bd118e70eeba
axg4975 <axg4975@rit.edu>
2018-02-26 06:31:20 +0000

    https to http is just not allowed
    Next TODO is to make flask app on RasberryPi https, which I am working on figuring out. I will get there, can't tonight.

--- RPiApp/app.py
+++ RPiApp/app.py
@@ -83,10 +83,7 @@ def motor_output(percent):
         # Wait before taking the next step...this controls rotation speed
         time.sleep(WaitTime)
 
-# If we're running this script directly, this portion executes. The Flask
-#  instance runs with the given parameters. Note that the "host=0.0.0.0" part
-#  is essential to telling the system that we want the app visible to the
-#  outside world.
+# The "host=0.0.0.0" part is essential to telling the system that we want the app visible to the
+# outside world.
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-    # app.run()
+    app.run(ssl_context='adhoc', host='0.0.0.0', port=5000)
