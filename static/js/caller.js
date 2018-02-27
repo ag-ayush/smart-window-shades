a2311248b89e9e7b87399d26d83f2559cf2ac583
axg4975 <axg4975@rit.edu>
2018-02-27 00:42:55 +0000

    Trying to fix access-control-allow-origin error

--- static/js/caller.js
+++ static/js/caller.js
@@ -2,12 +2,10 @@ $('#100').click(function(){
     $.ajax({
         type: "POST",
         url: "https://129.21.76.35:5000/gpio/100/",
-
-        success: function() {
-            console.log("SUCCESS");
-        },
-        error: function() {
-            console.log("FAIL");
-        }
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function() { console.log("SUCCESS");},
+        error: function() { console.log("FAIL");},
+        beforeSend: setHeader
     });
 });
