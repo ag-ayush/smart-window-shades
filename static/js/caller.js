db8f75c9633bc514b3584e85b8685ab2b7a6ab39
axg4975 <axg4975@rit.edu>
2018-02-27 01:13:27 +0000

    wecallback thing there, but syntax error as well?

--- static/js/caller.js
+++ static/js/caller.js
@@ -2,10 +2,9 @@ $('#100').click(function(){
     $.ajax({
         type: "POST",
         url: "https://129.21.76.35:5000/gpio/100/",
-        crossDomain: true,
 //      Needed because of callback gibberish in url at the end.
         jsonp : false,
-        jsonpCallback: '',
+        jsonpCallback: false,
 
         dataType: 'jsonp',
         success: function() { console.log("SUCCESS");},
