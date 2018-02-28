360f600dc9bb1e6ac2a1996f8a744d1afd71c108
axg4975 <axg4975@rit.edu>
2018-02-28 20:52:41 +0000

    function works!
    This will aid eventually in shifting from buttons to may be a slider, depends
    on what people prefer

--- static/js/caller.js
+++ static/js/caller.js
@@ -1,52 +1,24 @@
-$('#100').click(function(e){
-    $.ajax({
-        type: "POST",
-        url: "http://129.21.76.35:5000/gpio/100/",
-        success: function() { console.alert("SUCCESS");},
-        error: function() { console.alert("FAIL");}
-    });
-});
-
-
-for(var i = 0; i < 99; ++i) {
+//Creates a click event for buttons 0 to 100
+for(var i = 0; i < 101; ++i) {
     (function (i) {
         var value = '#' + i.toString();
-        console.log("Value: " + value)
         $(value).click(function(e){
             $.ajax({
                 type: "POST",
                 url: "http://129.21.76.35:5000/gpio/" + i.toString() + "/",
-                success: function() { console.alert("SUCCESS");},
-                error: function() { console.alert("FAIL");}
+                success: function() { console.log("Success");},
+                error: function() { console.log("Fail");}
             });
         });
     })(i);
 }
 
-
-//$('#50').click(function(e){
+/* Original */
+//$('#100').click(function(e){
 //    $.ajax({
 //        type: "POST",
-//        url: "http://129.21.76.35:5000/gpio/50/",
-//        success: function() { console.log("SUCCESS");},
-//        error: function() { console.log("FAIL");}
-//    });
-//});
-//
-//$('#25').click(function(e){
-//    $.ajax({
-//        type: "POST",
-//        url: "http://129.21.76.35:5000/gpio/25/",
-//        success: function() { console.log("SUCCESS");},
-//        error: function() { console.log("FAIL");}
-//    });
-//});
-//
-//$('#0').click(function(e){
-//    $.ajax({
-//        type: "POST",
-//        url: "http://129.21.76.35:5000/gpio/0/",
-//        success: function() { console.log("SUCCESS");},
-//        error: function() { console.log("FAIL");}
+//        url: "http://129.21.76.35:5000/gpio/100/",
+//        success: function() { console.alert("SUCCESS");},
+//        error: function() { console.alert("FAIL");}
 //    });
 //});
