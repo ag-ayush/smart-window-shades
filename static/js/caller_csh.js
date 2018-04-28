e286eb893c19be88b522f5b135c99ec2e4f9d966
axg4975 <axg4975@rit.edu>
2018-04-28 01:59:57 -0400

    fxing

--- static/js/caller_csh.js
+++ static/js/caller_csh.js
@@ -5,7 +5,7 @@ for(var i = 0; i < 101; ++i) {
         $(value).click(function(e){
             $.ajax({
                 type: "POST",
-                url: "https://agrpi.csh.rit.edu:5000/gpio/" + i.toString(),
+                url: "https://129.21.50.30:5000/gpio/" + i.toString(),
                 success: function() { console.log("Success");},
                 error: function() { console.log("Fail");}
             });
@@ -17,7 +17,7 @@ for(var i = 0; i < 101; ++i) {
 
 //Put shade height on the webpage
 function put_shade_height(){
-    $.get("https://agrpi.csh.rit.edu:5000/gpio/get/current", function(data, status){
+    $.get("https://129.21.50.30:5000/gpio/get/current", function(data, status){
                 //data is jsonp, I want to print the first value in it as html
                 var obj = JSON.parse(data);
                 $('#perc').html(obj["data"] + " %");
@@ -30,7 +30,7 @@ put_shade_height();
 $('#up').click(function(e){
     $.ajax({
         type: "POST",
-        url: "https://agrpi.csh.rit.edu:5000/gpio/set/current/up",
+        url: "https://129.21.50.30:5000/gpio/set/current/up",
         success: function() { console.log("SUCCESS");},
         error: function() { console.log("FAIL");}
     });
@@ -40,7 +40,7 @@ $('#up').click(function(e){
 $('#down').click(function(e){
     $.ajax({
         type: "POST",
-        url: "https://agrpi.csh.rit.edu:5000/gpio/set/current/down",
+        url: "https://129.21.50.30:5000/gpio/set/current/down",
         success: function() { console.log("SUCCESS");},
         error: function() { console.log("FAIL");}
     });
@@ -53,7 +53,7 @@ $('#steps').click(function(e){
     var val = $("#input_count").val()
     $.ajax({
         type: "POST",
-        url: "https://agrpi.csh.rit.edu:5000/gpio/set/steps/" + val,
+        url: "https://129.21.50.30:5000/gpio/set/steps/" + val,
         success: function() { console.log("SUCCESS");},
         error: function() { console.log("FAIL");}
     });
@@ -64,7 +64,7 @@ $('#move').click(function(e){
     var val = $("#input_steps").val()
     $.ajax({
         type: "POST",
-        url: "https://agrpi.csh.rit.edu:5000/gpio/move/" + val,
+        url: "https://129.21.50.30:5000/gpio/move/" + val,
         success: function() { console.log("SUCCESS");},
         error: function() { console.log("FAIL");}
     });
